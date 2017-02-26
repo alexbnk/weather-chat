882362787d20a31c9fe4a16bcecbcd59b16f20a2
alex <alexbnk@gmail.com>
2017-02-26 15:11:51 +0200

    addded function randomId in new file , it creates random ids with the provided length as parameter

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// posts array here
+//var posts =[];
+
+
+var weatherChat = {
+ posts : [{ pid:"111111",puser: "Alex", pcity:"Raanana", ptemp:22,pcomments: [{cid:"3763",cuser:"John",ctext:"just a comment"}]}],
+ getPosts : function () {
+   return this.posts;
+ }
+
+
+
+
+
+ };
